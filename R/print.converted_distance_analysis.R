810ae701f95eab831dbe62d4375b42f36674b73b
David Lawrence Miller <github@ninepointeightone.net>
2015-08-29 15:50:18 -0400

    print the filter used on a converted analysis

--- R/print.converted_distance_analysis.R
+++ R/print.converted_distance_analysis.R
@@ -5,10 +5,11 @@
 #' @export
 print.converted_distance_analysis <- function(x, ...){
 
-  cat("Model name :", x$name,"\n")
-  cat("ID         :", x$ID,"\n")
+  cat("Model name  :", x$name, "\n")
+  cat("ID          :", x$ID, "\n")
+  cat("Data filter :", x$filter, "\n")
 
   cat("mrds call  :\n\n")
-  cat(x$call,"\n")
+  cat(x$call, "\n")
 
 }
