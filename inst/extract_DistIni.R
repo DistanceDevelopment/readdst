98c328d24b2cbf9db5797e18d4b958fa684f3366
David Lawrence Miller <github@ninepointeightone.net>
2015-12-04 11:50:03 -0500

    extract conversion units

--- inst/extract_DistIni.R
+++ inst/extract_DistIni.R
@@ -2,12 +2,18 @@
 
 library(Hmisc)
 
+# get the parameter codes and descriptions
 param_str <- mdb.get("DistIni.mdb","ProjectSettings")
 param_str <- param_str[param_str$Section == "PrmDesc",]
-
-
 param_str$Section <- NULL
-
 print(param_str)
+
+
+# Unit conversions
+unitc <- mdb.get("DistIni.mdb","ProjectSettingsNumber")
+unitc <- unitc[unitc$Section %in% c("LinUnit", "AreaUnit", "AngUnit"),]
+unitc$Section <- NULL
+print(unitc)
+
 
 
