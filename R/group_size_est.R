4eb0f53746f7512df97289452bfad4c2227d47c9
David Lawrence Miller <github@ninepointeightone.net>
2015-12-15 18:00:11 -0500

    group size parsing didn't work for GX etc

--- R/group_size_est.R
+++ R/group_size_est.R
@@ -3,23 +3,22 @@
 #' Distance for Windows includes a few different methods for accounting for group (or cluster) size (at the adundance/density estimation stage). These include using the mean group size for all observations or using a regression of size against distance or log size against distance.
 #'
 #' @param data the data for this analysis
-#' @param model_definition a model definition
+#' @param group_size the \code{group_size} element of an analysis object
 #' @param model a fitted model
 #' @return a single the estimated cluster size, or \code{NULL} if there were no instructions on how to estimate group/cluster size
 #' @author David L Miller
-group_size_est <- function(data, model_definition, model){
+group_size_est <- function(data, group_size, model){
 
 ### Need to also take into account SIZE p. 328
 
   # grab the cluster term if it exists
-  if(is.null(model_definition$Estimate$Cluster) || is.null(data$size)){
+  if(is.null(group_size) || is.null(data$size)){
     return(NULL)
   }
-  cb <- model_definition$Estimate$Cluster
 
   # get the width and truncate
-  if(!is.null(cb$Width)){
-    data <- data[data$distance <= cb$Width, ]
+  if(!is.null(group_size$Width)){
+    data <- data[data$distance <= group_size$Width, ]
   }else{
     data <- data[data$distance <= model$meta.data$width, ]
   }
@@ -27,9 +26,9 @@ group_size_est <- function(data, model_definition, model){
 ###
 
   # if MEAN exists
-  if(!is.null(cb$Mean)){
+  if(!is.null(group_size$Mean)){
     cluster <- mean(data$size)
-  }else if(!is.null(cb$Bias)){
+  }else if(!is.null(group_size$Bias)){
     # do the size bias regression
     # from the distance manual
     # X       Regress cluster size against distance
@@ -41,22 +40,24 @@ group_size_est <- function(data, model_definition, model){
     reg_data <- data[, c("size", "distance")]
     response <- "size"
     explanatory <- "distance"
+    pred_data <- data.frame(distance=0)
 
     # if we need to transform size...
-    if(cb$Bias == "XLOG"){
+    if(group_size$Bias %in% c("GXLOG", "XLOG")){
       reg_data$log_size <- log(reg_data$size)
       response <- "log_size"
     }
     # if we need g(x) rather than distance
-    if(cb$Bias == "GXLOG"){
-      reg_data$gx <- predict(model)
+    if(group_size$Bias %in% c("GX", "GXLOG")){
+      reg_data$gx <- predict(model)[[1]]
       explanatory <- "gx"
+      pred_data <- data.frame(gx=1)
     }
 
     # do a regression!
-    size_lm <- lm(as.formula(paste0(response, "~", explanatory)), data=reg_data)
+    size_lm <- lm(paste0(response, "~", explanatory), data=reg_data)
     # predict the cluster size at zero distance
-    cluster <- predict(size_lm, data.frame(distance=0))
+    cluster <- predict(size_lm, pred_data)
   }
 
   return(cluster)
